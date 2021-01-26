71d42f51762220772d6b1550cb52c9d0a661354e
Tobey <cyberfly@email.cz>
2021-01-26 18:13:45 +0100

    initial layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log('hello');
+const browser = require('./browser');
+const consts = require('./consts');
+const crawling = require('./crawling');
+const performance = require('./performance');
+const platform = require('./platform');
+const queue = require('./queue');
+const social = require('./social');
+const stealth = require('./stealth');
+const time = require('./time');
+const transform = require('./transform');
+
+const log = () => {};
+const sleep = () => {};
+
+module.exports = {
+    log,
+    sleep,
+    browser,
+    consts,
+    crawling,
+    platform,
+    queue,
+    social,
+    stealth,
+    performance,
+    time,
+    transform,
+};
+
+// const {tools} = require('apify-tools')
+// tools.log()
+// tools.sleep()
+// tools.browser.login()
+// tools.crawling.enqueueLinks()
+// tools.performance.intervalPushData()
